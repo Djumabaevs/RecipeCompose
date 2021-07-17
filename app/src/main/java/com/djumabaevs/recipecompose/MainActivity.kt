ac3cbb33fca5c8c51449f7cbba43bb34d1cd6e5a
Djumabaevs <djumabaevb@gmail.com>
2021-07-17 13:44:34 +0600

    row

--- app/src/main/java/com/djumabaevs/recipecompose/MainActivity.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/MainActivity.kt
@@ -3,15 +3,15 @@ package com.djumabaevs.recipecompose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -27,43 +27,62 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            LazyColumn(modifier = Modifier.background(color = Color(0xFFf2f2f2))) {
-                items(2) {
-                    Image(
-                        painterResource(
-                            id = R.drawable.happy_meal
-                        ),
-                        modifier = Modifier.height(300.dp),
 
-                        contentScale = ContentScale.Crop,
-                        contentDescription = "meal picture"
-                    )
 
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        Text(
-                            text = "Happy Meal",
-                            style = TextStyle(
-                                fontSize = 26.sp
-                            )
-                        )
-                        Spacer(modifier = Modifier.padding(top = 10.dp))
-                        Text(
-                            text = "800 calories",
-                            style = TextStyle(
-                                fontSize = 17.sp
-                            )
-                        )
-                        Spacer(modifier = Modifier.padding(top = 10.dp))
-                        Text(
-                            text = "$5.99",
-                            style = TextStyle(
-                                color = Color(0xFF85bb65),
-                                fontSize = 17.sp
-                            )
-                        )
-                    }
-                }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .border(border = BorderStroke(width = 1.dp, color = Color.Black))
+            ) {
+
+                Text(
+                    text = "ITEM1",
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
             }
+
+
+            /*        LazyColumn(modifier = Modifier
+                        .background(color = Color(0xFFf2f2f2))
+                        .fillMaxSize()) {
+                        items(1) {
+                            Image(
+                                painterResource(
+                                    id = R.drawable.happy_meal
+                                ),
+                                modifier = Modifier.height(300.dp),
+
+                                contentScale = ContentScale.Crop,
+                                contentDescription = "meal picture"
+                            )
+
+                            Column(modifier = Modifier.padding(16.dp)) {
+                                Text(
+                                    text = "Happy Meal",
+                                    style = TextStyle(
+                                        fontSize = 26.sp
+                                    )
+                                )
+                                Spacer(modifier = Modifier.padding(top = 10.dp))
+                                Text(
+                                    text = "800 calories",
+                                    style = TextStyle(
+                                        fontSize = 17.sp
+                                    )
+                                )
+                                Spacer(modifier = Modifier.padding(top = 10.dp))
+                                Text(
+                                    text = "$5.99",
+                                    style = TextStyle(
+                                        color = Color(0xFF85bb65),
+                                        fontSize = 17.sp
+                                    )
+                                )
+                            }
+                        }
+                    }*/
         }
     }
 }
