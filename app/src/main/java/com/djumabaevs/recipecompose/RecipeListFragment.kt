2e8fb85b16a2626267857832a9175ae5cfbb049d
Djumabaevs <djumabaevb@gmail.com>
2021-07-17 15:44:40 +0600

    factory in AndroidView

--- app/src/main/java/com/djumabaevs/recipecompose/RecipeListFragment.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/RecipeListFragment.kt
@@ -4,8 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 
 class RecipeListFragment: Fragment() {
@@ -16,11 +27,34 @@ class RecipeListFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return ComposeView(requireContext()).apply {
+        val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
+        view.findViewById<ComposeView>(R.id.compose_view).setContent {
+
+            Column(modifier = Modifier
+                .border(border = BorderStroke(1.dp, Color.Black))
+                .padding(16.dp)) {
+
+                Text(text = "This is a composable inside a fragment")
+                Spacer(modifier = Modifier.padding(16.dp))
+                CircularProgressIndicator()
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(text = "Nice")
+                Spacer(modifier = Modifier.padding(16.dp))
+
+                val customView = HorizontalDottedProgress(LocalContext.current)
+                AndroidView(factory = {customView}) {
+
+                }
+            }
+
+        }
+        //Three other ways to use compose
+
+     /*   return ComposeView(requireContext()).apply {
             setContent {
                 Text(text = "Recipe List Fragment")
             }
-        }
+        }*/
 
 /*
         val view = ComposeView(requireContext())
@@ -34,6 +68,8 @@ class RecipeListFragment: Fragment() {
        /* val view = inflater.inflate(R.layout.fragment_recipe_list,
         container, false)
         return view*/
+
+        return view
     }
 
 }
