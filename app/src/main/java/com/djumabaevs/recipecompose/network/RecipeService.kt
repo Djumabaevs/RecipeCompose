44da9285521c0678275fa42e1d5134c1943a9aea
Djumabaevs <djumabaevb@gmail.com>
2021-07-18 14:57:38 +0600

    suspend fun get

--- app/src/main/java/com/djumabaevs/recipecompose/network/RecipeService.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/network/RecipeService.kt
@@ -1,5 +1,6 @@
 package com.djumabaevs.recipecompose.network
 
+import com.djumabaevs.recipecompose.network.model.RecipeNetworkEntity
 import com.djumabaevs.recipecompose.network.responses.RecipeSearchResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -9,9 +10,15 @@ interface RecipeService {
 
     @GET("search")
     suspend fun search(
-        @Header("authorization") token: String,
+        @Header("Authorization") token: String,
         @Query("page") page: Int,
         @Query("query") query: String
     ): RecipeSearchResponse
+
+    @GET("get")
+    suspend fun get(
+        @Header("Authorization") token: String,
+        @Query("id") id: Int
+    ): RecipeNetworkEntity
 
 }
