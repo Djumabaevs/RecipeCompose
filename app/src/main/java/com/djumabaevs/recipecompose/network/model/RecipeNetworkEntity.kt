f77998f2dab0c11784b8d954a7bb9b3f83bc2f9f
Djumabaevs <djumabaevb@gmail.com>
2021-07-18 13:04:19 +0600

    recipe network entity

---
+++ app/src/main/java/com/djumabaevs/recipecompose/network/model/RecipeNetworkEntity.kt
@@ -0,0 +1,40 @@
+package com.djumabaevs.recipecompose.network.model
+
+import com.google.gson.annotations.SerializedName
+
+class RecipeNetworkEntity(
+
+    @SerializedName("pk")
+    val pk: Int? = null,
+
+    @SerializedName("title")
+    val title: String? = null,
+
+    @SerializedName("publisher")
+    var publisher: String? = null,
+
+    @SerializedName("featured_image")
+    var featuredImage: String? = null,
+
+    @SerializedName("rating")
+    var rating: Int? = 0,
+
+    @SerializedName("source_url")
+    var sourceUrl: String? = null,
+
+    @SerializedName("description")
+    var description: String? = null,
+
+    @SerializedName("cooking_instructions")
+    var cookingInstructions: String? = null,
+
+    @SerializedName("ingredients")
+    var ingredients: List<String>? = null,
+
+    @SerializedName("date_added")
+    var dateAdded: String? = null,
+
+    @SerializedName("date_updated")
+    var dateUpdated: String? = null,
+
+)
