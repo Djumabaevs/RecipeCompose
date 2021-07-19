073aab23f933d52a7fa5affd64240a62abee16ac
Djumabaevs <djumabaevb@gmail.com>
2021-07-19 11:54:08 +0600

    change namimg funs in mapper dto class

--- app/src/main/java/com/djumabaevs/recipecompose/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/repository/RecipeRepositoryImpl.kt
@@ -1,8 +1,16 @@
 package com.djumabaevs.recipecompose.repository
 
 import com.djumabaevs.recipecompose.domain.model.Recipe
+import com.djumabaevs.recipecompose.domain.util.DomainMapper
+import com.djumabaevs.recipecompose.network.RecipeService
+import com.djumabaevs.recipecompose.network.model.RecipeDto
+import com.djumabaevs.recipecompose.network.model.RecipeDtoMapper
 
-class RecipeRepositoryImpl: RecipeRepository {
+class RecipeRepositoryImpl(
+    private val recipeService: RecipeService,
+    private val mapper: RecipeDtoMapper
+//    private val mapper: DomainMapper<RecipeDto, Recipe>
+): RecipeRepository {
     override suspend fun search(token: String, page: Int, query: String): List<Recipe> {
 
     }
