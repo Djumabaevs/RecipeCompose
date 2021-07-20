382644394634e49ae3e96da1a655c28f4dd630a3
Djumabaevs <djumabaevb@gmail.com>
2021-07-20 11:08:02 +0600

    provide recipe dto mapper in network module

--- app/src/main/java/com/djumabaevs/recipecompose/di/NetworkModule.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/di/NetworkModule.kt
@@ -1,12 +1,21 @@
 package com.djumabaevs.recipecompose.di
 
+import com.djumabaevs.recipecompose.network.model.RecipeDtoMapper
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRecipeMapper(): RecipeDtoMapper {
+        return RecipeDtoMapper()
+    }
 
 
 
--- app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListViewModel.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListViewModel.kt
@@ -3,6 +3,7 @@ package com.djumabaevs.recipecompose.presentation.ui.recipeList
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 
+
 class RecipeListViewModel @ViewModelInject constructor(
     private val randomString: String
 ): ViewModel() {
