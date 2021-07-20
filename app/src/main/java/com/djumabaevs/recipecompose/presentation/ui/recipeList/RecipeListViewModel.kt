bcb8649ca7a01cd2193fdad9790c5c0b2a36ed34
Djumabaevs <djumabaevb@gmail.com>
2021-07-20 12:56:44 +0600

    mutable live data

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListViewModel.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListViewModel.kt
@@ -4,8 +4,10 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.djumabaevs.recipecompose.domain.model.Recipe
 import com.djumabaevs.recipecompose.repository.RecipeRepository
+import kotlinx.coroutines.launch
 import javax.inject.Named
 
 
@@ -17,6 +19,15 @@ class RecipeListViewModel @ViewModelInject constructor(
     private val _recipes: MutableLiveData<List<Recipe>> = MutableLiveData()
     val recipes: LiveData<List<Recipe>> get() = _recipes
 
-
+    init {
+        viewModelScope.launch {
+            val result = repository.search(
+                token = token,
+                page = 1,
+                query = "chicken"
+            )
+            _recipes.value = result
+        }
+    }
 
 }
