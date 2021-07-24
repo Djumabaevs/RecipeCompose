bd452807bf3c9c16039cd11abc464e89346cfc06
Djumabaevs <djumabaevb@gmail.com>
2021-07-24 13:49:38 +0600

    CircularProgressIndicator

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/CircularIndeterminateProgressBar.kt
@@ -1,8 +1,27 @@
 package com.djumabaevs.recipecompose.presentation.components
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier.*
+
 
 @Composable
-fun CircularIndeterminateProgressBar() {
-    
+fun CircularIndeterminateProgressBar(
+    isDisplayed: Boolean,
+) {
+    if(isDisplayed) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(35.dp),
+            horizontalArrangement = Arrangement.Center) {
+
+            CircularProgressIndicator(
+                color = MaterialTheme.colors.primary
+            )
+        }
+    }
 }
