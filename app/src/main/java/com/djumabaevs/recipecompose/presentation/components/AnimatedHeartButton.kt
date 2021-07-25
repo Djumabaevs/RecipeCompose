e1cc31e6848d114970bb034e15743af90031ef68
Djumabaevs <djumabaevb@gmail.com>
2021-07-25 12:01:16 +0600

    animate dp as state

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/AnimatedHeartButton.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/AnimatedHeartButton.kt
@@ -1,2 +1,42 @@
 package com.djumabaevs.recipecompose.presentation.components
 
+import android.util.Log
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+object HeartAnimationDefinition {
+
+    enum class HeartButtonState {
+        IDLE, ACTIVE
+    }
+
+    val idleIconSize = 50.dp
+    val expandedIconSize = 80.dp
+
+}
+
+@Composable
+fun AnimatedHeartButton(
+    modifier: Modifier,
+    buttonState: MutableState<HeartAnimationDefinition.HeartButtonState>,
+    onToggle: () -> Unit)
+{
+
+    val size by animateDpAsState(
+        // Animation won't work if target value is always equal, this may be a jetpack compose bug?
+        if(buttonState.value == HeartAnimationDefinition.HeartButtonState.ACTIVE) 50.1.dp else 50.dp,
+        animationSpec = keyframes {
+            durationMillis = 500
+            HeartAnimationDefinition.expandedIconSize.at(100)
+            HeartAnimationDefinition.idleIconSize.at(200)
+        },
+        finishedListener = {
+            Log.d("DEBUG", "TEST ANIMATION")
+        }
+    )
