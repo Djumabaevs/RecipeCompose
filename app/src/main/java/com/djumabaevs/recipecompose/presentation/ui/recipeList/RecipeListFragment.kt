c083481b3aef97c3d8376cd902b7a1db35e3a42f
Djumabaevs <djumabaevb@gmail.com>
2021-07-25 11:47:43 +0600

    animated heart button

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListFragment.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListFragment.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.djumabaevs.recipecompose.presentation.components.CircularIndeterminateProgressBar
+import com.djumabaevs.recipecompose.presentation.components.PulsingDemo
 import com.djumabaevs.recipecompose.presentation.components.RecipeCard
 import com.djumabaevs.recipecompose.presentation.components.SearchAppBar
 import dagger.hilt.android.AndroidEntryPoint
@@ -51,6 +52,7 @@ class RecipeListFragment : Fragment() {
 
                 val loading = viewModel.loading.value
 
+                PulsingDemo()
 
                 Column {
 
