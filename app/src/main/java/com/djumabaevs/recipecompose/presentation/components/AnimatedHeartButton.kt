f347cc832abe4c755949ae3e4265b7a1035c7601
Djumabaevs <djumabaevb@gmail.com>
2021-07-25 12:02:41 +0600

    private composable heart button

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/AnimatedHeartButton.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/AnimatedHeartButton.kt
@@ -4,10 +4,17 @@ import android.util.Log
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 object HeartAnimationDefinition {
@@ -40,3 +47,35 @@ fun AnimatedHeartButton(
             Log.d("DEBUG", "TEST ANIMATION")
         }
     )
+
+    HeartButton(
+        modifier = modifier,
+        buttonState = buttonState,
+        onToggle = onToggle,
+        size = size
+    )
+}
+
+@Composable private fun HeartButton(
+    modifier: Modifier,
+    buttonState: MutableState<HeartAnimationDefinition.HeartButtonState>,
+    onToggle: () -> Unit,
+    size: Dp
+) {
+    if(buttonState.value == HeartAnimationDefinition.HeartButtonState.ACTIVE) {
+        Image(
+            imageVector = Icons.Default.Favorite,
+            modifier = modifier
+                .size(size)
+                .clickable(onClick = onToggle),
+            contentDescription = "")
+    }
+    else {
+        Image(
+            imageVector = Icons.Default.FavoriteBorder,
+            modifier = modifier
+                .size(size)
+                .clickable(onClick = onToggle),
+            contentDescription = "")
+    }
+}
