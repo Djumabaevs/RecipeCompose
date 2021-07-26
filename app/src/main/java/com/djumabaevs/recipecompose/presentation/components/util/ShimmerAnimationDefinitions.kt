72e3f82fcabe05f7830fef5c87b0830c8156df02
Djumabaevs <djumabaevb@gmail.com>
2021-07-26 13:31:41 +0600

    remember infinite transition

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/util/ShimmerAnimationDefinitions.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/util/ShimmerAnimationDefinitions.kt
@@ -1,6 +1,16 @@
 package com.djumabaevs.recipecompose.presentation.components.util
 
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.runtime.Composable
+
 object ShimmerAnimationDefinitions {
 
+    enum class AnimationState {
+        START, END
+    }
+}
 
+@Composable
+fun ShimmerAnimation() {
+    val transition = rememberInfiniteTransition()
 }
