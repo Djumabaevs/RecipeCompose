aeed3a67816e65f801595dde114deb9ad0a0dee4
Djumabaevs <djumabaevb@gmail.com>
2021-07-28 12:16:52 +0600

    cancel active job and create new one

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/util/SnackbarController.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/util/SnackbarController.kt
@@ -3,11 +3,12 @@ package com.djumabaevs.recipecompose.presentation.components.util
 import androidx.compose.material.ScaffoldState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class SnackbarController(
     private val scope: CoroutineScope
 ) {
-    private val snackbarJob: Job? = null
+    private var snackbarJob: Job? = null
 
     fun getScope() = scope
 
@@ -16,7 +17,31 @@ class SnackbarController(
         message: String,
         actionLabel: String,
     ) {
+        if(snackbarJob == null) {
+            snackbarJob = scope.launch {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = message,
+                    actionLabel = actionLabel
+                )
+            }
+            cancelActiveJob()
+        } else {
+            cancelActiveJob()
+            snackbarJob = scope.launch {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = message,
+                    actionLabel = actionLabel
+                )
+            }
+            cancelActiveJob()
+        }
+    }
 
+    private fun cancelActiveJob() {
+        snackbarJob?.let { job ->
+            job.cancel()
+            snackbarJob = Job()
+        }
     }
 
 }
