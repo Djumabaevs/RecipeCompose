80cca12c04ad7b3e6ad9fdb0a64fa3648b3a0775
Djumabaevs <djumabaevb@gmail.com>
2021-08-01 12:06:53 +0600

    add 4 const val to view models

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListViewModel.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListViewModel.kt
@@ -16,6 +16,11 @@ import com.djumabaevs.recipecompose.presentation.ui.recipeList.RecipeListEvent.*
 
 const val PAGE_SIZE = 30
 
+const val STATE_KEY_PAGE = "recipe.state.page.key"
+const val STATE_KEY_QUERY = "recipe.state.query.key"
+const val STATE_KEY_LIST_POSITION = "recipe.state.query.list_position"
+const val STATE_KEY_SELECTED_CATEGORY = "recipe.state.query.selected_category"a
+
 class RecipeListViewModel @ViewModelInject constructor(
     private val repository: RecipeRepository,
     @Named("auth_token") private val token: String,
