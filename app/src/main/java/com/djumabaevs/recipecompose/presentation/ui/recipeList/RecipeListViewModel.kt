1bfcac2387d6e7613eba403f8f9c912e70dc019f
Djumabaevs <djumabaevb@gmail.com>
2021-08-01 11:48:56 +0600

    assisted saved state handle

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListViewModel.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipeList/RecipeListViewModel.kt
@@ -3,11 +3,9 @@ package com.djumabaevs.recipecompose.presentation.ui.recipeList
 import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.djumabaevs.recipecompose.domain.model.Recipe
 import com.djumabaevs.recipecompose.repository.RecipeRepository
 import com.djumabaevs.recipecompose.util.TAG
@@ -20,7 +18,8 @@ const val PAGE_SIZE = 30
 
 class RecipeListViewModel @ViewModelInject constructor(
     private val repository: RecipeRepository,
-    @Named("auth_token") private val token: String
+    @Named("auth_token") private val token: String,
+    @Assisted private val savedStateHandle: SavedStateHandle
 ): ViewModel() {
 
     val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf())
