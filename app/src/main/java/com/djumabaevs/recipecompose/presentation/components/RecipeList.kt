fc6e26a49be65cc33ded7d8abfa1ee43af17843c
Djumabaevs <djumabaevb@gmail.com>
2021-08-01 14:22:07 +0600

    pass funs instead of objects when possible to params

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/RecipeList.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/RecipeList.kt
@@ -6,17 +6,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.djumabaevs.recipecompose.domain.model.Recipe
 import com.djumabaevs.recipecompose.presentation.components.util.ShimmerAnimation
 import com.djumabaevs.recipecompose.presentation.ui.recipeList.PAGE_SIZE
 import com.djumabaevs.recipecompose.presentation.ui.recipeList.RecipeListEvent
 
 @Composable
 fun RecipeList(
-
+    loading: Boolean,
+    recipes: List<Recipe>,
+    onChangeRecipeScrollPosition: (Int) -> Unit,
+    page: Int,
+    onTriggerEvent: (RecipeListEvent) -> Unit,
+    scaffoldState: ScaffoldState
 ) {
     Box(modifier = Modifier
         .fillMaxSize()
@@ -38,10 +45,10 @@ fun RecipeList(
                 itemsIndexed(
                     items = recipes
                 ) { index, recipe ->
-                    viewModel.onChangeRecipeScrollPosition(index)
+                    onChangeRecipeScrollPosition(index)
 
                     if((index + 1) >= (page * PAGE_SIZE) && !loading) {
-                        viewModel.onTriggerEvent(RecipeListEvent.NewPageEvent)
+                        onTriggerEvent(RecipeListEvent.NewPageEvent)
                     }
 
                     RecipeCard(recipe = recipe, onClick = {})
