81f90aafd0ff3fb4cbeb3d399307a6e0ddda67a2
Djumabaevs <djumabaevb@gmail.com>
2021-08-02 10:31:14 +0600

    wrap content width with alignment start

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/RecipeView.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/RecipeView.kt
@@ -1,12 +1,11 @@
 package com.djumabaevs.recipecompose.presentation.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
@@ -37,9 +36,22 @@ fun RecipeView(
                 }
             }
             Column(
-                modifier = Modifier.fillMaxWidth().padding(8.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
             ) {
-
+                recipe.title?.let { title ->
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)) {
+                        Text(
+                            text = title,
+                            modifier = Modifier
+                                .fillMaxWidth(0.85f)
+                                .wrapContentWidth(Alignment.Start)
+                        )
+                    }
+                }
 
             }
         }
