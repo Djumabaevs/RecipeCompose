81b8ee826220dafa33fada21a785d6735d6b0355
Djumabaevs <djumabaevb@gmail.com>
2021-08-02 10:27:14 +0600

    add column with title and score

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/RecipeView.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/RecipeView.kt
@@ -1,12 +1,15 @@
 package com.djumabaevs.recipecompose.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.djumabaevs.recipecompose.domain.model.Recipe
 import com.djumabaevs.recipecompose.util.DEFAULT_RECIPE_IMAGE
@@ -28,9 +31,16 @@ fun RecipeView(
                         contentDescription = "Image",
                         modifier = Modifier
                             .fillMaxWidth()
-                            .height(260.dp)
+                            .height(260.dp),
+                        contentScale = ContentScale.Crop
                     )
                 }
+            }
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(8.dp)
+            ) {
+
+
             }
         }
     }
