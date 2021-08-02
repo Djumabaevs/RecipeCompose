013121ef6e5cdc19886b27fd9e3af87d9f90e17e
Djumabaevs <djumabaevb@gmail.com>
2021-08-02 10:23:58 +0600

    lazy column with 50 items

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/RecipeView.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/RecipeView.kt
@@ -1,16 +1,37 @@
 package com.djumabaevs.recipecompose.presentation.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.unit.dp
 import com.djumabaevs.recipecompose.domain.model.Recipe
+import com.djumabaevs.recipecompose.util.DEFAULT_RECIPE_IMAGE
+import com.djumabaevs.recipecompose.util.LoadPicture
 
 @Composable
 fun RecipeView(
     recipe: Recipe
 ) {
-    LazyColumn(modifier = Modifier.fillMaxWidth()) {
-
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        items(50) {
+            recipe.featuredImage?.let { url ->
+                val image = LoadPicture(url = url, defaultImage = DEFAULT_RECIPE_IMAGE).value
+                image?.let { img ->
+                    Image(
+                        bitmap = img.asImageBitmap(),
+                        contentDescription = "Image",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(260.dp)
+                    )
+                }
+            }
+        }
     }
 }
