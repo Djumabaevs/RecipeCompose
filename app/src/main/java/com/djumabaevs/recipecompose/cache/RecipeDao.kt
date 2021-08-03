759e6a2d4097724d8193ef90ef41a35dc870683d
Djumabaevs <djumabaevb@gmail.com>
2021-08-03 13:27:34 +0600

    Retrieve recipes for a particular page

--- app/src/main/java/com/djumabaevs/recipecompose/cache/RecipeDao.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/cache/RecipeDao.kt
@@ -26,4 +26,21 @@ interface RecipeDao {
     @Query("DELETE FROM recipes WHERE id = :primaryKey")
     suspend fun deleteRecipe(primaryKey: Int): Int
 
+    /**
+     * Retrieve recipes for a particular page.
+     * Ex: page = 2 retrieves recipes from 30-60.
+     * Ex: page = 3 retrieves recipes from 60-90
+     */
+    @Query("""
+        SELECT * FROM recipes 
+        WHERE title LIKE '%' || :query || '%'
+        OR ingredients LIKE '%' || :query || '%'  
+        ORDER BY date_updated DESC LIMIT :pageSize OFFSET ((:page - 1) * :pageSize)
+        """)
+    suspend fun searchRecipes(
+        query: String,
+        page: Int,
+        pageSize: Int = RECIPE_PAGINATION_PAGE_SIZE
+    ): List<RecipeEntity>
+
 }
