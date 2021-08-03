e68ba7b6c6a075ef236a3d174482417528653753
Djumabaevs <djumabaevb@gmail.com>
2021-08-03 13:26:53 +0600

    suspend fun get recipe by id

--- app/src/main/java/com/djumabaevs/recipecompose/cache/RecipeDao.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/cache/RecipeDao.kt
@@ -2,6 +2,7 @@ package com.djumabaevs.recipecompose.cache
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.djumabaevs.recipecompose.cache.model.RecipeEntity
 
 @Dao
@@ -13,6 +14,16 @@ interface RecipeDao {
     @Insert
     suspend fun insertRecipes(recipes: List<RecipeEntity>): LongArray
 
+    @Query("SELECT * FROM recipes WHERE id = :id")
+    suspend fun getRecipeById(id: Int): RecipeEntity?
 
+    @Query("DELETE FROM recipes WHERE id IN (:ids)")
+    suspend fun deleteRecipes(ids: List<Int>): Int
+
+    @Query("DELETE FROM recipes")
+    suspend fun deleteAllRecipes()
+
+    @Query("DELETE FROM recipes WHERE id = :primaryKey")
+    suspend fun deleteRecipe(primaryKey: Int): Int
 
 }
