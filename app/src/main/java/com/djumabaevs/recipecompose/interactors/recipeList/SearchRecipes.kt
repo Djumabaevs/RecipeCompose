5eaf77cff382ac3356873f9b5ebede2d960a5543
Djumabaevs <djumabaevb@gmail.com>
2021-08-05 12:33:39 +0600

    get recipes from cache or search new ones

--- app/src/main/java/com/djumabaevs/recipecompose/interactors/recipeList/SearchRecipes.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/interactors/recipeList/SearchRecipes.kt
@@ -6,6 +6,7 @@ import com.djumabaevs.recipecompose.domain.data.DataState
 import com.djumabaevs.recipecompose.domain.model.Recipe
 import com.djumabaevs.recipecompose.network.RecipeService
 import com.djumabaevs.recipecompose.network.model.RecipeDtoMapper
+import com.djumabaevs.recipecompose.util.RECIPE_PAGINATION_PAGE_SIZE
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -35,11 +36,37 @@ class SearchRecipes(
             //if there is a network connection
             if(isNetworkAvailable) {
                 //Convert:NetworkRecipeEntity -> Entity -> RecipeCacheEntity
+                val recipes = getRecipesFromNetwork(
+                    query = query,
+                    page = page,
+                    token = token
+                )
+                //insert into cache
+                recipeDao.insertRecipes(entityMapper.toEntityList(recipes))
             }
+            //query the cache
+            val cacheResult = if(query.isBlank()) {
+                recipeDao.getAllRecipes(
+                    page = page,
+                    pageSize = RECIPE_PAGINATION_PAGE_SIZE
+                )
+            } else {
+                recipeDao.searchRecipes(
+                    query = query,
+                    pageSize = RECIPE_PAGINATION_PAGE_SIZE,
+                    page = page
+                )
+            }
+            //emit List<Recipe> from cache
+            val list = entityMapper.fromEntityList(cacheResult)
+            emit(DataState.success(list))
+        } catch (e: Exception) {
+            emit(DataState.error<List<Recipe>>(e.message?: "Unknown error"))
         }
 
     }
 
+    //WARING: this will throw exception if there is no network connection.
     private suspend fun getRecipesFromNetwork(
         token: String,
         page: Int,
