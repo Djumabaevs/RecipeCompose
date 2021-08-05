3ec088307600888f1ad8da664a94957df921fc24
Djumabaevs <djumabaevb@gmail.com>
2021-08-05 10:35:16 +0600

    cache module provideDb

---
+++ app/src/main/java/com/djumabaevs/recipecompose/di/CacheModule.kt
@@ -0,0 +1,27 @@
+package com.djumabaevs.recipecompose.di
+
+import androidx.room.Room
+import com.djumabaevs.recipecompose.cache.database.AppDatabase
+import com.djumabaevs.recipecompose.presentation.BaseApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CacheModule {
+
+    @Singleton
+    @Provides
+    fun provideDb(app: BaseApplication): AppDatabase {
+    return Room
+        .databaseBuilder(app, AppDatabase::class.java, AppDatabase.DATABASE_NAME)
+        .fallbackToDestructiveMigration()
+        .build()
+    }
+
+    @Singleton
+
+}
