c470ae51f2ccf2ecd2d96eb064f58e3f22fc7d62
Djumabaevs <djumabaevb@gmail.com>
2021-08-05 11:51:44 +0600

    does network have internet

--- app/src/main/java/com/djumabaevs/recipecompose/interactors/app/DoesNetworkHaveInternet.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/interactors/app/DoesNetworkHaveInternet.kt
@@ -1,2 +1,25 @@
 package com.djumabaevs.recipecompose.interactors.app
 
+import android.util.Log
+import com.djumabaevs.recipecompose.util.TAG
+import java.io.IOException
+import java.net.InetSocketAddress
+import javax.net.SocketFactory
+
+object DoesNetworkHaveInternet {
+
+    //Make sure to execute this on a background thread.
+    fun execute(socketFactory: SocketFactory): Boolean {
+        return try {
+            Log.d(TAG, "execute: Pinging GOOGLE.")
+            val socket = socketFactory.createSocket()?: throw IOException("socket is null.")
+            socket.connect(InetSocketAddress("8.8.8.8", 53), 1500)
+            socket.close()
+            Log.d(TAG, "execute: Success ping.")
+            true
+        }catch (e: Exception) {
+            Log.e(TAG, "execute: No internet connection: ${e}")
+            false
+        }
+    }
+}
