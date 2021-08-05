aa6ca985e0ca2a48320178149064efa76fa0a27b
Djumabaevs <djumabaevb@gmail.com>
2021-08-05 12:14:38 +0600

    emit list of recipes from cache

--- app/src/main/java/com/djumabaevs/recipecompose/interactors/recipeList/RestoreRecipes.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/interactors/recipeList/RestoreRecipes.kt
@@ -1,5 +1,48 @@
 package com.djumabaevs.recipecompose.interactors.recipeList
 
-class RestoreRecipes () {
+import com.djumabaevs.recipecompose.cache.RecipeDao
+import com.djumabaevs.recipecompose.cache.model.RecipeEntityMapper
+import com.djumabaevs.recipecompose.domain.data.DataState
+import com.djumabaevs.recipecompose.domain.model.Recipe
+import com.djumabaevs.recipecompose.util.RECIPE_PAGINATION_PAGE_SIZE
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.lang.Exception
+
+class RestoreRecipes(
+    private val recipeDao: RecipeDao,
+    private val entityMapper: RecipeEntityMapper
+) {
+    fun execute(
+        page: Int,
+        query: String
+    ): Flow<DataState<List<Recipe>>> = flow {
+        try{
+            emit(DataState.loading())
+
+            //just to show pagination, api is fast
+            delay(1000)
+
+            //query the cache
+            val cacheResult = if (query.isBlank()) {
+                recipeDao.restoreAllRecipes(
+                    pageSize = RECIPE_PAGINATION_PAGE_SIZE,
+                    page = page
+                )
+            } else {
+                recipeDao.restoreRecipes(
+                    query = query,
+                    pageSize = RECIPE_PAGINATION_PAGE_SIZE,
+                    page = page
+                )
+            }
+            //emit List<Recipe> from cache
+            val list = entityMapper.fromEntityList(cacheResult)
+            emit(DataState.success(list))
+        } catch (e: Exception) {
+            emit(DataState.error<List<Recipe>>(e.message?: "Unknown error"))
+        }
+    }
 
 }
