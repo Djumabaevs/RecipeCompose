f6618b47e9c8e812f803ac33fc37492ed125606d
Djumabaevs <djumabaevb@gmail.com>
2021-08-07 13:45:28 +0600

    modifiers

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/ConnectivityMonitor.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/ConnectivityMonitor.kt
@@ -2,9 +2,13 @@ package com.djumabaevs.recipecompose.presentation.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ConnectivityMonitor(
@@ -12,7 +16,11 @@ fun ConnectivityMonitor(
 ) {
     Column(modifier = Modifier.fillMaxWidth()) {
         Text(
-            text =
+            text = "No network available!",
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.h6
         )
     }
 }
