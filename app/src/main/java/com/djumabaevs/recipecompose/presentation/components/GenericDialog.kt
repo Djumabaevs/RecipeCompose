d421388d76a0c13dfe53cb1de7abb213d93233f2
Djumabaevs <djumabaevb@gmail.com>
2021-08-07 14:16:08 +0600

    add composables and data classes to generic dialog

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/GenericDialog.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/GenericDialog.kt
@@ -1,7 +1,13 @@
 package com.djumabaevs.recipecompose.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun GenericDialog(
@@ -12,6 +18,42 @@ fun GenericDialog(
     positiveAction: PositiveAction?,
     negativeAction: NegativeAction?,
 ) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = onDismiss,
+        title = { Text(title) },
+        text = {
+            if (description != null) {
+                Text(text = description)
+            }
+        },
+        buttons = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.End,
+            ) {
+                if(negativeAction != null){
+                    Button(
+                        modifier = Modifier.padding(end = 8.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.onError),
+                        onClick = negativeAction.onNegativeAction
+                    ) {
+                        Text(text = negativeAction.negativeBtnTxt)
+                    }
+                }
+                if(positiveAction != null){
+                    Button(
+                        modifier = Modifier.padding(end = 8.dp),
+                        onClick = positiveAction.onPositiveAction,
+                    ) {
+                        Text(text = positiveAction.positiveBtnTxt)
+                    }
+                }
+            }
+        }
+    )
 
 }
 
