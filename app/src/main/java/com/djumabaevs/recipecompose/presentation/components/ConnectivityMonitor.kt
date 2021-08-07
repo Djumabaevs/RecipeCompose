b204ba57055e01c5d2853debe81434d726b432f4
Djumabaevs <djumabaevb@gmail.com>
2021-08-07 13:43:07 +0600

    add text to conect monit

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/components/ConnectivityMonitor.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/components/ConnectivityMonitor.kt
@@ -1,2 +1,18 @@
 package com.djumabaevs.recipecompose.presentation.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ConnectivityMonitor(
+    isNetworkAvailable: Boolean
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text =
+        )
+    }
+}
