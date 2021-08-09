d472815a7845b7ff1f9e292a2409fbc0fcdfba93
Djumabaevs <djumabaevb@gmail.com>
2021-08-09 10:57:12 +0600

    save available network connections to a set

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/util/ConnectionLiveData.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/util/ConnectionLiveData.kt
@@ -1,4 +1,15 @@
 package com.djumabaevs.recipecompose.presentation.util
 
+val TAG = "C- Manager"
+
+/**
+ * Save all available networks with an internet connection to a set (@validNetworks).
+ * As long as the size of the set > 0, this LiveData emits true.
+ * MinSdk = 21.
+ *
+ * Inspired by:
+ * https://github.com/AlexSheva-mason/Rick-Morty-Database/blob/master/app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/networking/ConnectionLiveData.kt
+ */
+
 class ConnectionLiveData {
 }
