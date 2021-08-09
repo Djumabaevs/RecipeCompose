3ec463df8d81a3c8b30535375982b1a9f057c662
Djumabaevs <djumabaevb@gmail.com>
2021-08-09 12:10:33 +0600

    register and unregister observers with lifecycleOwners

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/util/ConnectivityManager.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/util/ConnectivityManager.kt
@@ -1,6 +1,8 @@
 package com.djumabaevs.recipecompose.presentation.util
 
 import android.app.Application
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LifecycleOwner
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -9,4 +11,19 @@ class ConnectivityManager
 @Inject constructor(application: Application)
 {
     private val connectionLiveData = ConnectionLiveData(application)
+
+    //observe this in ui
+    val isNetworkAvailable = mutableStateOf(false)
+
+    fun registerConnectionObserver(lifecycleOwner: LifecycleOwner) {
+        connectionLiveData.observe(lifecycleOwner, { isConnected ->
+            isConnected?.let {
+                isNetworkAvailable.value = it
+            }
+        })
+    }
+
+    fun unregisterConnectionObserver(lifecycleOwner: LifecycleOwner) {
+        connectionLiveData.removeObservers(lifecycleOwner)
+    }
 }
