1acc0538b8532a165ad58ae77307598e8753158b
Djumabaevs <djumabaevb@gmail.com>
2021-08-09 11:44:01 +0600

    object of cm with two override methods

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/util/ConnectionLiveData.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/util/ConnectionLiveData.kt
@@ -28,4 +28,35 @@ class ConnectionLiveData(context: Context): LiveData<Boolean>() {
     private val cm = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
     private val validNetworks: MutableSet<Network> = HashSet()
 
+    private fun checkValidNetworks() {
+        postValue(validNetworks.size > 0)
+    }
+
+    override fun onActive() {
+        networkCallback = createN
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NET_CAPABILITY_INTERNET)
+            .build()
+        cm.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    override fun onInactive() {
+        cm.unregisterNetworkCallback(networkCallback)
+    }
+
+    private fun createNetworkCallback() = object: ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            Log.d(TAG, "onAvailable: ${network}")
+        }
+        /*
+         If the callback was registered with registerNetworkCallback() it will be called for each network which no longer satisfies the criteria of the callback.
+         Source: https://developer.android.com/reference/android/net/ConnectivityManager.NetworkCallback#onLost(android.net.Network)
+        */
+        override fun onLost(network: Network) {
+            super.onLost(network)
+        }
+    }
+
+
+
 }
