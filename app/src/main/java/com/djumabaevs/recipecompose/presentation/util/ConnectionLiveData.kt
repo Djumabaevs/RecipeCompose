5cba77bd7fee7171420634ce8940b063ca725147
Djumabaevs <djumabaevb@gmail.com>
2021-08-09 11:02:51 +0600

    MutableSet<Network> = HashSet()

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/util/ConnectionLiveData.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/util/ConnectionLiveData.kt
@@ -1,5 +1,16 @@
 package com.djumabaevs.recipecompose.presentation.util
 
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkRequest
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.LiveData
+import java.util.HashSet
+
 val TAG = "C- Manager"
 
 /**
@@ -11,5 +22,10 @@ val TAG = "C- Manager"
  * https://github.com/AlexSheva-mason/Rick-Morty-Database/blob/master/app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/networking/ConnectionLiveData.kt
  */
 
-class ConnectionLiveData {
+class ConnectionLiveData(context: Context): LiveData<Boolean>() {
+
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
+    private val cm = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val validNetworks: MutableSet<Network> = HashSet()
+
 }
