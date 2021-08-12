8f8f2b9691b59e54fe63f18eb47578b37ba958b4
Djumabaevs <djumabaevb@gmail.com>
2021-08-12 11:16:33 +0600

    composable detail recipe screen with viewmodel as parameter

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipe/RecipeDetailScreen.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipe/RecipeDetailScreen.kt
@@ -1,2 +1,22 @@
 package com.djumabaevs.recipecompose.presentation.ui.recipe
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecipeDetailScreen(
+    isDarkTheme: Boolean,
+    isNetworkAvailable: Boolean,
+    recipeId: Int?,
+    viewModel: RecipeDetailViewModel
+) {
+    if(recipeId == null) {
+        TODO("show invalid recipe")
+    } else {
+        // fire a one-off event to get the recipe from api
+        val onLoad = viewModel.onLoad.value
+        if(!onLoad) {
+            viewModel.onLoad.value = true
+            viewModel.onTriggerEvent(RecipeEvent.GetRecipeEvent(recipeId))
+        }
+    }
+}
