d1c958617439d45054103af314d845f884d177e4
Djumabaevs <djumabaevb@gmail.com>
2021-08-12 11:21:46 +0600

    add app theme experimental

--- app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipe/RecipeDetailScreen.kt
+++ app/src/main/java/com/djumabaevs/recipecompose/presentation/ui/recipe/RecipeDetailScreen.kt
@@ -1,7 +1,13 @@
 package com.djumabaevs.recipecompose.presentation.ui.recipe
 
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import com.djumabaevs.recipecompose.presentation.theme.AppTheme
 
+@ExperimentalMaterialApi
+@ExperimentalComposeUiApi
 @Composable
 fun RecipeDetailScreen(
     isDarkTheme: Boolean,
@@ -17,6 +23,21 @@ fun RecipeDetailScreen(
         if(!onLoad) {
             viewModel.onLoad.value = true
             viewModel.onTriggerEvent(RecipeEvent.GetRecipeEvent(recipeId))
+        }
+
+        val loading = viewModel.loading.value
+        val recipe = viewModel.recipe.value
+        val dialogQueue = viewModel.dialogQueue
+        val scaffoldState = rememberScaffoldState()
+
+        AppTheme(
+            darkTheme = isDarkTheme,
+            isNetworkAvailable = isNetworkAvailable,
+            displayProgressBar = loading,
+            scaffoldState = scaffoldState,
+            dialogQueue = dialogQueue.queue.value
+        ) {
+
         }
     }
 }
